0811da9eb318f4e482a19fe192ea2aa6c7e3a514
Davendra Seunarine Maharaj <davendramaharaj0@gmail.com>
2024-02-01 15:56:36 -0500

    implemented embedding table for emb_chrg

--- L1DeepMetv2-Golden/l1deepmetv2_compute.cc
+++ L1DeepMetv2-Golden/l1deepmetv2_compute.cc
@@ -2,7 +2,7 @@
 
 /** intermediate variables in the forward function of GraphMetNetwork */
 float emb_cont[NUM_NODES][HIDDEN_DIM/2];
-float emb_charge[NUM_NODES][HIDDEN_DIM/4];
+float emb_chrg[NUM_NODES][HIDDEN_DIM/4];
 
 
 /** implementation of torch.nn.ELU() 
@@ -21,7 +21,7 @@ void GraphMetNetworkLayer(float x_cont[NUM_NODES][CONT_DIM], int x_cat[NUM_NODES
     /** emb_cont = self.embed_continuous(x_cont) */
     memset(emb_cont, 0, NUM_NODES * HIDDEN_DIM/2 * sizeof(float));
     /** shape: (NUM_NODES, CONT_DIM) * (CONT_DIM, HIDDEN_NUM//2)  = (NUM_NODES, HIDDEN_NUM//2) */
-    for(int i = 0; i < NUM_NODES - 1; i++)
+    for(int i = 0; i < NUM_NODES; i++)
     {
         for(int j = 0; j < HIDDEN_DIM/2; j++)
         {
@@ -38,4 +38,18 @@ void GraphMetNetworkLayer(float x_cont[NUM_NODES][CONT_DIM], int x_cat[NUM_NODES
             emb_cont[i][j] = ELU(emb_cont[i][j], 1.0);
         }
     }
+
+    /** emb_chrg = self.embed_charge(x_cat[:, 1] + 1) */
+    for (int i = 0; i < NUM_NODES; i++)
+    {
+        /** get the indx */
+        int idx = x_cat[i][1] + 1;
+
+        for (int j = 0; j < HIDDEN_DIM/4; j++)
+        {
+            emb_chrg[i][j] = graphmet_embed_charge_weight[idx][j];
+        }
+    }
+
+    /** pdg_remap = torch.abs(x_cat[:, 0]) */
 }
