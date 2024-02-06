e66c495925dbe7973a502858870bfac4add891e8
Davendra Seunarine Maharaj <davendramaharaj0@gmail.com>
2024-02-05 22:38:23 -0500

    implemented embed_categorical linear layer

--- L1DeepMetv2-Golden/l1deepmetv2_compute.cc
+++ L1DeepMetv2-Golden/l1deepmetv2_compute.cc
@@ -4,6 +4,7 @@
 float emb_cont[NUM_NODES][HIDDEN_DIM/2];
 float emb_chrg[NUM_NODES][HIDDEN_DIM/4];
 float emb_pdg[NUM_NODES][HIDDEN_DIM/4];
+float emb_cat[NUM_NODES][HIDDEN_DIM/2];
 
 /** implementation of torch.nn.ELU() 
  * 
@@ -21,11 +22,10 @@ void GraphMetNetworkLayer(float x_cont[NUM_NODES][CONT_DIM], int x_cat[NUM_NODES
     /** emb_cont = self.embed_continuous(x_cont) */
     memset(emb_cont, 0, NUM_NODES * HIDDEN_DIM/2 * sizeof(float));
     /** shape: (NUM_NODES, CONT_DIM) * (CONT_DIM, HIDDEN_NUM//2)  = (NUM_NODES, HIDDEN_NUM//2) */
-    for(int i = 0; i < NUM_NODES; i++)
+    for (int i = 0; i < NUM_NODES; i++)
     {
         for(int j = 0; j < HIDDEN_DIM/2; j++)
         {
-            emb_cont[i][j] = 0.0;
             for(int k = 0; k < CONT_DIM; k++)
             {
                 emb_cont[i][j] += x_cont[i][k] * graphmet_embed_continuous_0_weight[j][k];
@@ -83,6 +83,34 @@ void GraphMetNetworkLayer(float x_cont[NUM_NODES][CONT_DIM], int x_cat[NUM_NODES
         for (int j = 0; j < HIDDEN_DIM/4; j++)
         {
             emb_pdg[i][j] = graphmet_embed_pdgid_weight[idx][j];
+        }
+    }
+
+    /** emb_cat = self.embed_categorical(torch.cat([emb_chrg, emb_pdg], dim=1)) 
+     * 
+     * Shape: (NUM_NODES, HIDDEN_DIM/2) * (2*HIDDEN_DIM/4, HIDDEN_DIM/2) = (NUM_NODES, HIDDEN_DIM/2)
+    */
+    memset(emb_cat, 0, NUM_NODES * HIDDEN_DIM/2 * sizeof(float));
+    for (int i = 0; i < NUM_NODES; i++)
+    {
+        for (int j = 0; j < HIDDEN_DIM/2; j++)
+        {
+            for (int k = 0; k < HIDDEN_DIM/2; k++)
+            {
+                if (k < HIDDEN_DIM/4)
+                {
+                    emb_cat[i][j] += emb_chrg[i][k] * graphmet_embed_categorical_0_weight[j][k];
+                }
+                else{
+                    emb_cat[i][j] += emb_pdg[i][k - HIDDEN_DIM/4] * graphmet_embed_categorical_0_weight[j][k];
+                }
+            }
+
+            /** add the categorical bias */
+            emb_cat[i][j] += graphmet_embed_categorical_0_bias[j];
+
+            /** apply torch.nn.ELU() to each element */
+            emb_cat[i][j] = ELU(emb_cat[i][j], 1.0);
         }
     }
 
